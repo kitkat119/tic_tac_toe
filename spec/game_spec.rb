41395b9f8e04863b0cc6773c3604406740e4e3e4
kitkat119 <gprydal@yahoo.co.uk>
2017-10-16 15:33:54 +0100

    tests switching turns in game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,23 +7,29 @@ describe Game do
   let(:index) { 8 }
 
   it 'starts with two players' do
-    expect(game.players.length).to eq(2)
+    expect(game.players.length).to eq 2
   end
 
   it 'has a player 1' do
-    expect(game.player_1).to eq(player_1)
+    expect(game.player_1).to eq player_1
   end
 
   it 'has a player 2' do
-    expect(game.player_2).to eq(player_2)
+    expect(game.player_2).to eq player_2
   end
 
   it "starts with player 1's turn" do
-    expect(game.current_turn).to eq(player_1)
+    expect(game.current_turn).to eq player_1
   end
 
   it 'lets the player make a move' do
     expect(board).to receive(:update)
     game.make_move(player_1, index)
+  end
+
+  it 'switches the current player once a move has been made' do
+    allow(board).to receive(:update)
+    game.make_move(player_1, index)
+    expect(game.current_turn).to eq player_2
   end
 end
